e61edd97e096dd23e42c45a4dd1b46307ed21b1e
mkpatel-247 <meet.codes247@gmail.com>
2025-01-15 00:43:52 +0530

    Create centralize error handling and api response.

---
+++ backend/src/utils/apiError.js
@@ -0,0 +1,16 @@
+import { sendResponse } from "./responseHandler";
+
+/**
+ * Centralize the error handling use this function.
+ * use this function and pass any function into it.
+ * If any error occur from here it will throw.
+ * @param {*} fn - callback function
+ * @returns - send API response.
+ */
+export const catchAsync = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        return sendResponse(res, error.statusCode, error.message);
+    }
+};
